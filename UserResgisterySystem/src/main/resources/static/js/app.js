a521097e011693d3c0def0ba7eb4923991ac128e
VishalPawar1010 <vshalofficial@gmail.com>
2023-02-08 13:03:38 +0530

    solved angularJS routing issue

--- UserResgisterySystem/src/main/resources/static/js/app.js
+++ UserResgisterySystem/src/main/resources/static/js/app.js
@@ -2,16 +2,16 @@ var app = angular.module('userregistrationsystem', ['ngRoute', 'ngResource']);
 app.config(['$routeProvider',function($routeProvider) {
     $routeProvider
     .when('/list-all-users', {
-        templateUrl : '/template/listuser.html',
+        templateUrl : 'template/listuser.html',
         controller : 'listUserController'
     }).when('/register-new-user',{
-        templateUrl : '/template/userregistration.html',
+        templateUrl : 'template/userregistration.html',
         controller : 'registerUserController'
     }).when('/update-user/:id',{
         templateUrl : '/template/userupdation.html' ,
         controller : 'usersDetailsController'
     }).otherwise({
-        redirectTo : '/home',
+        redirectTo : '/',
         templateUrl : 'template/home.html',
     });
 }]);
